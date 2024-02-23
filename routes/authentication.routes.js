4cd7df211928d47afea12daf6c4f6f51c81fd3ae
alexis peloso <alexisthd1961@gmail.com>
2024-02-23 00:54:14 -0300

    subiendo correciones del sprint pasado (2)

--- routes/authentication.routes.js
+++ routes/authentication.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const authController = require("../controllers/authentication")
 
 router.get("/registro", authController.registro);
-router.get("/registro-segunda-parte", authController.registro1);
-router.get("/registro-tercera-parte", authController.registro2);   
+router.get("/registro-paso-2", authController.registro1);
+router.get("/registro-paso-3", authController.registro2);   
 
 router.get("/login", authController.login);
 
