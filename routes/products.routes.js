14ebe19977fecb59a929177bca68b4704d696720
alexis peloso <alexisthd1961@gmail.com>
2024-03-12 16:09:06 -0300

    subiendo progreso en detalle de producto

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const prodController = require("../controllers/products");
 
-router.get("/detalle", prodController.detail);
+router.get('/detalle/:id', prodController.detail);
 router.get("/registro-producto", prodController.registroProducto);
 router.get("/editarProducto", prodController.editarProducto)
 router.get("/lista-productos", prodController.listaProductos)
