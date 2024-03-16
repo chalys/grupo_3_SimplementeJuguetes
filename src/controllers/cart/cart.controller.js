4ecc8bf4beb53fa56fecdfb72db443ec237ebf59
alexis peloso <alexisthd1961@gmail.com>
2024-03-15 23:26:57 -0300

    resolviendo conflictos sprint 4

--- src/controllers/cart/cart.controller.js
+++ src/controllers/cart/cart.controller.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 module.exports = (req, res) =>{
-    const searchJSON = path.join(__dirname,"../../basesDeDatos/productos.json")
+    const searchJSON = path.join(__dirname,"../../data/productsDataBase.json")
     let products = JSON.parse(fs.readFileSync(searchJSON, 'utf-8'))
     const id = req.params.id;
     const searchProduct = products.find((p)=>{
     return p.id == id
     });
-    res.render("detail",{
+    res.render("cart",{
         p: searchProduct,
         ps: products,
     })
--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -2,5 +2,6 @@ module.exports = {
   detail: require("./detail.controller"),
   registroProducto: require("./registroProducto.controller"),
   listaProductos: require("./listaProductos.controller"),
-  editarProducto: require("./editarProducto.controller")
+  editarProducto: require("./editarProducto.controller"),
+  editsProducts: require("./editsProducts.controller")
   }
