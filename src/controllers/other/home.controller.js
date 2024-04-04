7b3bbe57a4444b83f2a577b0b4aa449003a13525
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2024-04-04 18:17:42 -0400

    add search form, controller, route  and result view

--- src/controllers/other/home.controller.js
+++ src/controllers/other/home.controller.js
@@ -2,6 +2,6 @@ const toThousand = (n) => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const { loadData } = require("../../data");
 
 module.exports = (req, res) =>{
-    const products = loadData();
+    const products = loadData("productsDataBase");
     res.render("other/home",{products:products, toThousand})
 }
