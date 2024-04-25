2e2cc4d7787cd5ef2f6d4de88d79bfddba8fba73
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2024-04-25 18:29:48 -0400

    update local develop

--- src/controllers/products/editarProducto.controller.js
+++ src/controllers/products/editarProducto.controller.js
@@ -1,7 +1,7 @@
-module.exports = (req, res) =>{
-    const products = require("../../basesDeDatos/productos.json")
-    const { id }=req.params;
-  const product =  products.find((p)=>p.id=== +id);
-    
-    res.render("editProduct",{product});
-}
+module.exports = (req, res) => {
+  const products = require("../../basesDeDatos/productos.json");
+  const { id } = req.params;
+  const product = products.find((p) => p.id === +id);
+
+  res.render("editProduct", { product });
+};
