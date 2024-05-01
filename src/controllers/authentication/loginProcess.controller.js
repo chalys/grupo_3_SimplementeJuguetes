8227c937dfef86a028fd70fbcc224f98ec562f86
alexis peloso <alexisthd1961@gmail.com>
2024-04-30 21:42:09 -0300

    refactorizacion login 1

--- src/controllers/authentication/loginProcess.controller.js
+++ src/controllers/authentication/loginProcess.controller.js
@@ -1,44 +1,40 @@
-const { loadData } = require("../../dataBase");
+// const { loadData } = require("../../dataBase");
+const db = require('../../dataBase/models')
 const { compareSync } = require("bcryptjs");
 module.exports = (req, res) => { 
 
     //Login
    
-   const { name, password, recuerdame } = req.body; // Cambiar 'nombre' a 'name'
-   const user = loadData("usuarios");
+   const { userName, password, recuerdame } = req.body; // Cambiar 'nombre' a 'name'
+   // const user = loadData("usuarios");
 
-   if(!name){ // Cambiar 'nombre' a 'name'
-      res.render("./authentication/errorAuth", 
+   if(!userName){ // Cambiar 'nombre' a 'name'
+    return  res.render("./authentication/errorAuth", 
       {mensage: "Debe ingresar su usuario",
        boton: "Intente nuevamente", 
        redireccion: "/autenticacion/login"
       });
    }
-   const userFind = user.find(u => u.userName === name)//.toLowerCase()) // Cambiar 'nombre' a 'name'
-
+   // const userFind = user.find(u => u.userName === name)//.toLowerCase()) // Cambiar 'nombre' a 'name'
+db.user.findOne({where:{userName,}}).then((userFind)=>{
    if(!userFind){
-      res.render("./authentication/errorAuth",
+   return  res.render("./authentication/errorAuth",
        {mensage: "El usuario no existe",
-       boton: "Registrese para continuar", 
+       boton: "Registrese para continuar",
        redireccion: "/autenticacion/registro"
-      
-      
       });
    }
-
    const passValid = compareSync(password, userFind.password)
-
    if(!passValid){
-      res.render("./authentication/errorAuth",
+   return   res.render("./authentication/errorAuth",
       {mensage: "El usuario o contraseña son incorrectos",
       boton: "Intente nuevamente", 
       redireccion: "/autenticacion/login"
       });
    }
-
    //Ingresar como usuario con session y recordar la session
-const {id,userName, email, province, locality, postal, street, streetNumber, floor, betweenSt1, betweenSt2, phoneNumber, indications, userPicture} = userFind;
-req.session.userLogin ={
+   const {id,name,userName, email, province, locality, postal, street, streetNumber, floor, betweenSt1, betweenSt2, phoneNumber, indications, userPicture} = userFind;
+   req.session.userLogin ={
    id,
    userName,
    name, // Esta es la variable 'name' que proviene de 'userFind'
@@ -55,12 +51,16 @@ req.session.userLogin ={
    phoneNumber,
    indications,
    userPicture
-}
+   }
+   if(recuerdame){ //remenber es una variable creada para representar un valor boleano.
+      res.cookie("userLogin", req.session.userLogin, {maxAge: 360 })
+   }
+   res.redirect("/")
+
+})
 
 
-if(recuerdame){ //remenber es una variable creada para representar un valor boleano.
-   res.cookie("userLogin", req.session.userLogin, {maxAge: 360 })
-}
 
-res.redirect("/")
+
+
 };
