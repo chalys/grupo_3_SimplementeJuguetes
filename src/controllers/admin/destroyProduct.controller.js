134248d36417aa76d6455cf5266279795a322af2
Rodrigo Toñanes <rodrigotonanes030@gmail.com>
2024-05-02 19:41:08 -0300

    delete product y editar perfil de usuario

--- src/controllers/admin/destroyProduct.controller.js
+++ src/controllers/admin/destroyProduct.controller.js
@@ -1,20 +1,45 @@
-const fs = require("fs")
-const path = require("path");
-const { loadData, saveData } = require("../../dataBase");
+// const fs = require("fs")
+// const path = require("path");
+// const { loadData, saveData } = require("../../dataBase");
+
+// module.exports = (req, res) => {
+//     const {id} = req.params;
+//     const products = loadData('products');
+
+//     const productsLessOne = products.filter(p => p.id !== +id);
+//     const productToDelete = products.find(p => p.id === +id)
+//     const imagePath = path.join(__dirname, `../../../public/images/products/${productToDelete.firstImg}`) 
+
+//         if (fs.existsSync(imagePath)){
+//             fs.unlinkSync(imagePath)
+//         }
+
+//     saveData(productsLessOne,"products")
+//     res.redirect("/admin/lista-productos")
+    
+// }
+
+
+
+const { dataBase} = require("../../dataBase/models");
+
 
 module.exports = (req, res) => {
-    const {id} = req.params;
-    const products = loadData('products');
-
-    const productsLessOne = products.filter(p => p.id !== +id);
-    const productToDelete = products.find(p => p.id === +id)
-    const imagePath = path.join(__dirname, `../../../public/images/products/${productToDelete.firstImg}`) 
-
-        if (fs.existsSync(imagePath)){
-            fs.unlinkSync(imagePath)
-        }
-
-    saveData(productsLessOne,"products")
+    const {id} = req.params
+dataBase.secondImg.destroy({
+    where:{
+        productId:id
+    }
+})
+.then(()=>{
+dataBase.product.destroy({
+    where: {
+        id
+    }
+})
+.then(()=>{
     res.redirect("/admin/lista-productos")
-    
+})
+})
+
 }
