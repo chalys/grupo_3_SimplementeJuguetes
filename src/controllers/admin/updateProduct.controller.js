42c45f8662cd45487e4dc886b5c5fecf2ef09381
alexis peloso <alexisthd1961@gmail.com>
2024-05-06 17:17:10 -0300

    resolviendo detalles en updateProduct

--- src/controllers/admin/updateProduct.controller.js
+++ src/controllers/admin/updateProduct.controller.js
@@ -1,20 +1,3 @@
-const db = require('../../dataBase/models');
-
-module.exports = (req, res) => {
-
-  const { id } = req.params; 
-  db.product.findByPk(id)  
-  .then((product) => {
-    res.render("admin/updateProduct", { product }, (err, contentView) => {
-      err && res.send(err.message);
-      res.render("partials/dashboard", { contentView });
-   })
-
- })
-};
-
-
-/*
 const { loadData } = require("../../dataBase");
 
 module.exports = (req, res) => {
@@ -25,7 +8,7 @@ module.exports = (req, res) => {
   const productFind = products.find((p) => p.id === +id);
   res.render("./admin/editProduct", { product:productFind });
 };
------------------------------------
+/*
   const product = products.find((p) => p.id === +id);
 
   res.render("admin/updateProduct", { product }, (err, contentView) => {
--- src/dataBase/models/product.js
+++ src/dataBase/models/product.js
@@ -15,40 +15,40 @@ module.exports = (sequelize, DataTypes) => {
   }
   product.init({
     name: DataTypes.STRING(100),
-    allowNull: false,
+    
     manufacturer: DataTypes.STRING(50),
-    allowNull: false,
+    
     mark: DataTypes.STRING(50),
-    allowNull: false,
+    
     sku: DataTypes.INTEGER,
-    allowNull: false,
+    
     available: DataTypes.BOOLEAN,
-    allowNull: false,
+    
     colection: DataTypes.STRING(50),
-    allowNull: false,
+    
     price: DataTypes.DECIMAL(10,2),
-    allowNull: false,
+    
     line: DataTypes.STRING(50),
-    allowNull: false,
+    
     character: DataTypes.STRING(50),
-    allowNull: false,
+    
     characterVersion: DataTypes.STRING(50),
-    allowNull: false,
+    
     minAge: DataTypes.STRING(30),
-    allowNull: false,
+    
     height: DataTypes.SMALLINT,
     depth: DataTypes.SMALLINT,
     width: DataTypes.SMALLINT,
     materials: DataTypes.STRING(50),
-    allowNull: false,
+    
     scale: DataTypes.STRING(30),
-    allowNull: false,
+    
     articulated: DataTypes.BOOLEAN,
     collectable: DataTypes.BOOLEAN,
     accessories: DataTypes.BOOLEAN,
     bobblehead: DataTypes.BOOLEAN,
     firstImg: DataTypes.STRING(100),
-    allowNull: false,
+    
     description: DataTypes.TEXT
   }, {
     sequelize,
