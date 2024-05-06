350428f1f5cfc32057c576109a9e584358a95ce8
CyranoBlack <alexandrevivartoledo@gmail.com>
2024-05-06 16:13:09 -0300

    subi cambios de refactorizacion

--- src/controllers/admin/storeProduct.controller.js
+++ src/controllers/admin/storeProduct.controller.js
@@ -1,3 +1,85 @@
+const db = require("../../dataBase/models");
+
+module.exports = (req, res) => {
+  const {
+    name,
+    description,
+    manufacturer,
+    mark,
+    sku,
+    available,
+    colection,
+    price,
+    line,
+    character,
+    characterVersion,
+    minAge,
+    height,
+    depth,
+    width,
+    materials,
+    scale,
+    articulated,
+    collectable,
+    accessories,
+    bobbleHead,
+  } = req.body;
+
+
+ db.product.create({
+    name: name.trim(),
+    description: description.trim(),
+    manufacturer: manufacturer.trim(),
+    mark: mark.trim(),
+    sku: +sku,
+    available: available?.trim(),
+    colection: colection.trim(),
+    price: +price,
+    line: line.trim(),
+    character: character.trim(),
+    characterVersion: characterVersion.trim(),
+    minAge: minAge?.trim(),
+    height: +height,
+    depth: +depth,
+    width: +width,
+    materials: materials?.trim(),
+    scale: +scale,
+    articulated: articulated?.trim(),
+    collectable: collectable?.trim(),
+    accessories: accessories?.trim(),
+    bobbleHead: bobbleHead?.trim(),
+    firstImg: req.files.firstImg?.length
+      ? req.files.firstImg[0]?.filename
+      : "default-image.jpg",
+    secondImg: newImages.length ? newImages : ["default-image.jpg"],
+  })
+  .then((product) => {
+    let newImages = [];
+    if (req.files?.imagesSecondary?.length) {
+        newImages = req.files.imagesSecondary?.map((img) => {
+            return {
+                file: img.filename,
+                productId: product.id
+            }
+        })
+        db.imagesecondary.bulkCreate(newImages)
+        .then(() => {
+            return res.redirect(`/admin/productos`)
+        })
+        .catch(err => {
+            res.send(err.message)
+        })
+    }
+    return res.redirect(`/admin/productos`)
+})
+
+  products.push(newProduct);
+  saveData(products, "products");
+  res.redirect(`/productos/detalle-producto/${newID}`);
+};
+
+
+/* anterior codigo 
 const { loadData, saveData } = require("../../dataBase");
 
 module.exports = (req, res) => {
@@ -65,3 +147,4 @@ module.exports = (req, res) => {
   saveData(products, "products");
   res.redirect(`/productos/detalle-producto/${newID}`);
 };
+*/
