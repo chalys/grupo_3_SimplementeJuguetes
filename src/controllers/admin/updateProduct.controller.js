54a5f11dc4966bbc80cf6db2051ddf3f25b46250
gastonfvargas <gastonfvargas@gmail.com>
2024-05-06 22:38:14 -0300

    Se realizaron unas modificaciones para probar el codigo de updateProduct

--- src/controllers/admin/updateProduct.controller.js
+++ src/controllers/admin/updateProduct.controller.js
@@ -8,6 +8,36 @@ module.exports = (req, res) => {
   const productFind = products.find((p) => p.id === +id);
   res.render("./admin/editProduct", { product:productFind });
 };
+
+/*  Se intento implementar pero por un error se deja la anterior version
+const db = require('../../dataBase/models')
+module.exports = (req,res) => {
+
+    const { id } = req.params;
+    const productPromise = db.product.findByPk(id)
+
+    Promise.all([productPromise])
+    .then(([product]) => {
+
+      res.render("/admin/updateProduct", {
+        product
+      }, (err,content) => {
+        if (err) {
+          console.error(err.message);
+          return res.send(err.message);
+        }
+        res.render('partials/dashboard', {
+          contentView:content
+        })
+}      )
+    })
+    
+}
+
+//------------------------------------------------------
+*/
+
+
 /*
   const product = products.find((p) => p.id === +id);
 
