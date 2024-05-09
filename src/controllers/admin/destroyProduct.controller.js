0527974ac3d4925614f7323a80ebc27f7b662467
Carlos Roberto Arando Llanos <chalys.all@gmail.com>
2024-05-09 10:47:29 -0400

    add API structure

--- src/controllers/admin/destroyProduct.controller.js
+++ src/controllers/admin/destroyProduct.controller.js
@@ -8,7 +8,7 @@
 
 //     const productsLessOne = products.filter(p => p.id !== +id);
 //     const productToDelete = products.find(p => p.id === +id)
-//     const imagePath = path.join(__dirname, `../../../public/images/products/${productToDelete.firstImg}`) 
+//     const imagePath = path.join(__dirname, `../../../public/images/products/${productToDelete.firstImg}`)
 
 //         if (fs.existsSync(imagePath)){
 //             fs.unlinkSync(imagePath)
@@ -16,30 +16,27 @@
 
 //     saveData(productsLessOne,"products")
 //     res.redirect("/admin/lista-productos")
-    
+
 // }
 
-
-
-const { dataBase} = require("../../dataBase/models");
-
+const { db } = require("../../dataBase/models");
 
 module.exports = (req, res) => {
-    const {id} = req.params
-dataBase.secondImg.destroy({
-    where:{
-        productId:id
-    }
-})
-.then(()=>{
-dataBase.product.destroy({
-    where: {
-        id
-    }
-})
-.then(()=>{
-    res.redirect("/admin/lista-productos")
-})
-})
-
-}
+  const { id } = req.params;
+  db.secondaryimage.destroy({
+      where: {
+        productId: id,
+      },
+    })
+    .then(() => {
+      db.Product
+        .destroy({
+          where: {
+            id,
+          },
+        })
+        .then(() => {
+          res.redirect("/admin/lista-productos");
+        });
+    });
+};
