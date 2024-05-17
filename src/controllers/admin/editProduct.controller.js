d57407d273a7ce1c88578a0630d1b90420fce412
alexis peloso <alexisthd1961@gmail.com>
2024-05-17 20:25:51 -0300

    formulario de edicion y de crear producto reformados

--- src/controllers/admin/editProduct.controller.js
+++ src/controllers/admin/editProduct.controller.js
@@ -1,54 +1,68 @@
 const { validationResult } = require("express-validator");
-//const { saveData, loadData } = require("../../dataBase");
+const { saveData, loadData } = require("../../dataBase");
 const db = require("../../dataBase/models");
 
 module.exports= (req,res) => {
 const errors = validationResult(req);
 const { id }= req.params
-//const products = loadData("products");
-
+const products = loadData("products");
 if (errors.isEmpty()) {
-    const {name,price,description,line,
-    characterVersion,available,manufacturer,
-    character, section, recSecondaryImage
-    } = req.body;
+  const {
+    name,
+    manufacturer,
+    mark,
+    sku,
+    available,
+    collection,
+    stock,
+    categoryId,
+    price,
+    line,
+    character,
+    characterVersion,
+    minAge,
+    height,
+    depth,
+    width,
+    materials,
+    scale,
+    articulated,
+    collectable,
+    accessories,
+    bobbleHead,
+    description,
+  } = req.body;
      
-    /*//A implementar ya que no contamos con este campo.
- 
-    db.secondaryimage.findAll({
-      productId: id
-    })
-    .then(images => {
-      let newImages = [];
-      if(req.files.secondaryimage?.length){
-        newImages = req.files.secondaryimage?.map((img) => img.filename);
-        return{
-          file: img.filename,
-          productId: +id
-        }    
-      }
 
-      if(recSecondaryImage === "on")
-        newImages = [
-          ...newImages,
-          ...images,
-      ]
-    })
-*/
-    db.product.update(
+    db.Product.update(
       {
-      name: name.trim(),
-      price: +price,
-      description: description.trim(),
-      line: line.trim(),
-      characterVersion: characterVersion.trim(),
-      available: available,
-      manufacturer: manufacturer.trim(),
-      character: character.trim(),
-      /*imagenPrincipal: req.files.imagenPrincipal[0]?.filename*/
-      articulated: section === "articulated",
-      collectable: section === "collectable",
-      includesAccessories: section === "includesAccessories"
+        name: name?.trim(),
+        manufacturer: manufacturer? manufacturer.trim(): "Sin especificar", //
+        mark: mark ? mark.trim() : "Sin clasificar", //
+        sku: +sku || 0,
+        available: available === "yes" ? 1 : 0,
+        collection: collection ? collection.trim() : "Sin clasificar", //
+        stock: +stock,
+        price: +price || 0,
+        line: line? line.trim(): 'sin linea', //
+        categoryId: categoryId? +categoryId : 0,
+        character: character ? character.trim() : "Sin clasificar", //
+        characterVersion: characterVersion ? characterVersion.trim() : "Sin clasificar", //
+        minAge: minAge ? minAge.trim() : "Edad Libre", //
+        height: +height || 0,
+        depth: +depth || 0,
+        width: +width || 0,
+        materials: materials ? materials.trim() : "Sin clasificar", //
+        scale: scale?scale.trim():"Sin escala", //
+        articulated: articulated === "yes" ? 1 : 0,
+        collectable: collectable === "yes" ? 1 : 0,
+        accessories: accessories === "yes" ? 1 : 0,
+        bobbleHead: bobbleHead === "yes" ? 1 : 0,
+        description: description ? description.trim() : "Este juguete no cuenta con una descripcion",//
+        firstImg: req.files.firstImg?.length
+          ? req.files.firstImg[0]?.filename
+          : "default-image.jpg",
+
     },{
       where: {
         id
@@ -88,10 +102,10 @@ if (errors.isEmpty()) {
       return p;
     });
 
-    saveData(productsMap,"products");
+    saveData(productsMap,"products");*/
 
-    // res.redirect("/admin/lista-productos");
-*/
+    res.redirect("/admin/lista-productos");
+
   } else {
     //const product = products.find((p) => p.id === +id);
     const errorsMapped = errors.mapped();
