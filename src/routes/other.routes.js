9f9c1f1f6b36fa27562ae762230ba3c87eb072b9
gastonfvargas <gastonfvargas@gmail.com>
2024-06-22 04:35:32 -0300

    Se suben cambios mejorando algunos errores esteticos de la vista en PC

--- src/routes/other.routes.js
+++ src/routes/other.routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {home, dashboard, search} = require("../controllers/other");
+const {home, dashboard, search, contact, aboutus} = require("../controllers/other");
 
 router.get("/", home);
 router.get("/home", home);
 router.get("/dashboard", dashboard);
 router.get('/buscar', search);
+router.get('/contacto', contact);
+router.get('/sobre-nosotros', aboutus);
 
 module.exports = router;
