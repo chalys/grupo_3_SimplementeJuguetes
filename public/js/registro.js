ac1d3e1da4cdb2197775461fee07defe250d54bf
gastonfvargas <gastonfvargas@gmail.com>
2024-07-15 22:12:34 -0300

    Se suben cambios esteticos

--- public/js/registro.js
+++ public/js/registro.js
@@ -1,14 +1,18 @@
-const toggle = document.querySelector(".toggle")
-const toggle2 = document.querySelector(".toggle2")
-const form1 = document.querySelector(".naregdes")
-const form2 = document.querySelector(".form")
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.querySelector(".toggle");
+    const toggle2 = document.querySelector(".toggle2");
+    const form1 = document.querySelector(".naregdes");
+    const form2 = document.querySelector(".form");
 
+    toggle.addEventListener("click", (event) => {
+        event.preventDefault(); // Previene el comportamiento predeterminado
+        form1.classList.toggle("f1");
+        form2.classList.toggle("f2");
+    });
 
-toggle.addEventListener("click", () => {
-    form1.classList.toggle("f1")
-    form2.classList.toggle("f2")
-})
-toggle2.addEventListener("click", () => {
-    form1.classList.toggle("f1")
-    form2.classList.toggle("f2")
-})
+    toggle2.addEventListener("click", (event) => {
+        event.preventDefault(); // Previene el comportamiento predeterminado
+        form1.classList.toggle("f1");
+        form2.classList.toggle("f2");
+    });
+});
